8433ba5eba99165477848979c57ca13c9ab05777
Venchislaw <venchislavcodes@gmail.com>
2024-06-13 16:38:55 +0300

    suffering IRL

--- Practice/RegularExpressions/literals_and_symb_cls.py
+++ Practice/RegularExpressions/literals_and_symb_cls.py
@@ -25,3 +25,19 @@ print(match)  # []
 
 # it doesn't work as it uses ^ anchor
 # it works only for beginning of the string, not for each word
+
+
+# in this case ^ works as "not" operator
+match = re.findall(r"[^0-9]", text)
+print(match)
+
+
+# Regular Expressions also have flags
+# Such flags can change behaviour of RE
+text = "안녕 세계"
+# in this case ^ works as "not" operator
+match = re.findall(r"\w", text, re.ASCII)
+print(match)  # []
+# but:
+match = re.findall(r"\w", text)
+print(match)  # ['안', '녕', '세', '계']
